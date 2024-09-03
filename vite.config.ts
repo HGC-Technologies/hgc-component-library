99d710686d77870c390b74505f5cbf7e58fbebdc
Qigong Zhu <philzhuqigong@gmail.com>
2024-09-03 15:25:55 -0400

    try with the library mode of vite
    vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    
+  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/components/index.ts'), // Entry point for your library
+      name: 'MyLibrary',
+      formats: ['es', 'cjs',],
+      fileName: (format) => `hgc-component-library.${format}.js`,
+    },
+    rollupOptions: {
+      // Make sure to externalize deps that shouldn't be bundled into your library
+      external: ['react', 'react-dom'],
+      output: {
+        // Provide global variables to use in the UMD build for externalized deps
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+  // build: {
+  //   outDir: 'dist',
+  //   sourcemap: true
+  // }
 })
+
+
