004a5fba86db2a943ff02d8298694661f2680742
lbigrasHGC <liam.bigras@WeAreHGC.com>
2024-11-19 10:30:18 -0500

    updated exports

--- dist/components/chart/index.d.ts
+++ dist/components/chart/index.d.ts
@@ -1,5 +1,5 @@
-import { ChartType, ChartOptions } from 'chart.js';
+import { ChartType, ChartOptions, ChartData } from 'chart.js';
 export { default as ReactBarChart } from './ReactBarChart';
 export { default as ReactPieChart } from './ReactPieChart';
 export { default as ComboBarLineChart } from './ComboBarLineChart';
-export { ChartType, ChartOptions };
+export { ChartType, ChartOptions, ChartData };
