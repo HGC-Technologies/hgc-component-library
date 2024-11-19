bf875bad7a0f463ff6ef4db9800f3810efc29337
lbigrasHGC <liam.bigras@WeAreHGC.com>
2024-11-19 10:46:34 -0500

    changed some things

--- src/components/chart/index.ts
+++ src/components/chart/index.ts
@@ -1,5 +1,6 @@
 import { ChartType, ChartOptions, ChartData } from 'chart.js';
+
 export { default as ReactBarChart } from './ReactBarChart';
 export { default as ReactPieChart } from './ReactPieChart';
-export { default as ComboBarLineChart} from "./ComboBarLineChart";
+export { default as ComboBarLineChart } from './ComboBarLineChart';
 export { ChartType, ChartOptions, ChartData }
